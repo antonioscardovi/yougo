f65f43e94bf240b318437d6e6414382150e13114
aaljukic <aljukicadis@yahoo.com>
2019-05-19 21:40:49 +0200

    Added controllers (User, Vehicle) boilerplate code

--- server/app/Http/Controllers/UsersController.php
+++ server/app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class VehiclesController extends Controller
+class UsersController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class VehiclesController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        //
     }
 
     /**
