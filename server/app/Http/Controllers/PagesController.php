ce587c32dd311f63fcd8e502bf51fb8fa40ac50e
aaljukic <aljukicadis@yahoo.com>
2019-05-19 20:34:44 +0200

    Added Vehicle controller

--- server/app/Http/Controllers/PagesController.php
+++ server/app/Http/Controllers/PagesController.php
@@ -6,23 +6,23 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function home(){
-        return view('home');
-    }
-
-    public function vehicles(){
-        return view('vehicles');
-    }
-
-    public function login(){
-        return view('login');
-    }
-
-    public function registration(){
-        return view('registration');
-    }
-
-    public function dashboard(){
-        return view('dashboard');
-    }
+//    public function home(){
+//        return view('home');
+//    }
+//
+//    public function vehicles(){
+//        return view('vehicles');
+//    }
+//
+//    public function login(){
+//        return view('login');
+//    }
+//
+//    public function registration(){
+//        return view('registration');
+//    }
+//
+//    public function dashboard(){
+//        return view('dashboard');
+//    }
 }
