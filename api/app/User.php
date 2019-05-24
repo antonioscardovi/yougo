2d6aa5e76d20dd6f94f3a2a082ca73d7c6208b62
aaljukic <aljukicadis@yahoo.com>
2019-05-24 11:07:23 +0200

    Fixes login and lastname field removed

--- api/app/User.php
+++ api/app/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'lastname',
+        //'lastname',
         'email',
         'password',
         'verification',
