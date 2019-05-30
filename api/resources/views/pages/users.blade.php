83be5cdbc1a8b7f2e560be66b40505ad7e715f34
Antonio Scardovi <antonio.scardovi@gmail.com>
2019-05-30 14:05:35 +0200

    tables

--- api/resources/views/pages/users.blade.php
+++ api/resources/views/pages/users.blade.php
@@ -1,16 +1,36 @@
 @extends('adminlte::page')
 
 @section('content')
-<div class="container">
-<div class="section">
-    <h1 class="title">All Users</h1>
+    <h1 class="title" style="margin-top:0;">All Users</h1>
 
-    @foreach($users as $user)
 
-    <a href="{{ url('users/'. $user->id) }}"><li>{{ $user->name }}</li></a>
+    <table id="table1" class="display cell-border table table-striped table-bordered" style="font-size: 2rem;">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td><button class="edit-modal btn btn-info"
+                        data-info="{{$user->id}},{{$user->name}}">
+                        <span class="glyphicon glyphicon-edit"></span> Edit
+                    </button>
+                    <button class="delete-modal btn btn-danger"
+                        data-info="{{$user->id}},{{$user->name}}">
+                        <span class="glyphicon glyphicon-trash"></span> Delete
+                    </button></td>
+                </tr>
+                {{-- <a href="{{ url('users/'. $user->id) }}"></a> --}} <!-- LINK ZA JEDNOG userA -->
+                @endforeach
+            </tbody>
+          </table>
 
-    @endforeach
-</div>
-</div>
+
 @endsection
 
