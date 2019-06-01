0c2bf71a48e4a4137e63dbfe3cfd8a5778bca62a
aaljukic <aljukicadis@yahoo.com>
2019-06-01 18:06:56 +0200

    added auth for each controller and page

--- api/app/Http/Controllers/Customer/CustomerBackendController.php
+++ api/app/Http/Controllers/Customer/CustomerBackendController.php
@@ -8,6 +8,11 @@ use App\Customer;
 
 class CustomerBackendController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $customers = Customer::all();
--- api/app/Http/Controllers/User/UserController.php
+++ api/app/Http/Controllers/User/UserController.php
@@ -9,6 +9,12 @@ use App\Http\Controllers\ApiController;
 
 class UserController extends ApiController
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
