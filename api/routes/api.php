2c48fd8e0c79e769d0173092c9a809d74121476f
aaljukic <aljukicadis@yahoo.com>
2019-06-06 16:13:24 +0200

    added new JWT - /me route need fix

--- api/routes/api.php
+++ api/routes/api.php
@@ -36,3 +36,22 @@ Route::resource('customers', 'Customer\CustomerController', ['except' => ['creat
  * */
 Route::resource('reservations', 'Reservation\ReservationController', ['except' => ['update', 'edit', 'create', 'store']]);
 //Route::post('reservations/{reservation}', 'Reservation\ReservationController@store')->name('reservations.store');
+
+
+
+/*
+ * JWT
+ * */
+Route::group([
+
+    //'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
