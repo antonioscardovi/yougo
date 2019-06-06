0878811b5982f77c1625c6414dbf7a385b5e74ca
aaljukic <aljukicadis@yahoo.com>
2019-06-06 16:13:24 +0200

    added new JWT - /me route need fix

--- api/app/Customer.php
+++ api/app/Customer.php
@@ -4,14 +4,39 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Customer extends Model
+class Customer extends Authenticatable implements JWTSubject
 {
 
     use Notifiable;
 
     const VERIFIED_CUSTOMER = '1';
     const UNVERIFIED_CUSTOMER = '0';
+
+    // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 
     /**
      * The attributes that are mass assignable.
